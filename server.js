bbb1ed365d122cb87b2afa0bf7e20c26b8633ad2
Galileo Guzmán <galileoguzman@gmail.com>
2016-01-18 00:01:04 -0600

    Adding query to PostgreSQL

--- server.js
+++ server.js
@@ -28,6 +28,14 @@ app.use(bodyParser.urlencoded());
 // in latest body-parser use like below.
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// ========================================
+// DATABASE SETUP
+// ========================================
+var pg = require('pg');
+var conString = "postgres://userrealtime:real_time00@localhost/realdb";
+
+
 // ========================================
 // CONFIG CONNECTION OF SOCKET IO
 // ========================================
@@ -48,15 +56,60 @@ app.get("/", function(req, res, next){
 	res.render('index');
 });
 
+// testing
+app.get("/hellow", function(req, res, next){
+	var data = {
+		name : "Galileo",
+		surname: "Guzmán",
+		age : 26,
+	};
+
+	res.json(data);
+});
+
 // ====================================================== //
 // == USER LOGIN
 // ====================================================== //
 app.post("/login", function(req, res, next){
 	console.log("Login Function Init");
-	console.log(req.body);
+	
+	// get vars from request
+	var reqData = req.body;
+	var reqUsername = reqData['username'];
+	var reqPassword = reqData['password'];
+
+	// query on postgresql db
+	var results = [];
+
+	pg.connect(conString, function(err, client, done) {
+		// Handle connection errors
+		if(err) {
+			done();
+			console.log(err);
+			return res.status(500).json({ success: false, data: err});
+		}
+
+		// SQL Query > Select Data
+		var query = client.query("SELECT name, surname FROM users WHERE username=($1) AND password=($2)", [reqUsername, reqPassword]);
+
+		// Stream results back one row at a time
+		query.on('row', function(row) {
+			results.push(row);
+		});
+
+		// After all data is returned, close connection and return results
+		query.on('end', function() {
+			done();
+			//return res.json(results);
+		});
+	});
+
+
+	console.log(results);
+
 	// Set response
-	res.setHeader('Content-Type', 'application/json');
-   	res.send(JSON.stringify(req.body));
+	//res.setHeader('Content-Type', 'application/json');
+   	res.json(results);
 });
 
 // ====================================================== //
